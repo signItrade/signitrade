6f87d4474a6d5b9e8ce259a69d40161c43eaefc1
unknown <Vikram@EOFFB075.devspad.nic.in>
2012-10-04 12:59:11 +0530

    ss

--- repositories/signitrade-data-repository/src/main/java/uk/co/signitrade/repository/data/model/Customer.java
+++ repositories/signitrade-data-repository/src/main/java/uk/co/signitrade/repository/data/model/Customer.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Customer implements java.io.Serializable {
 
 	/**
-	 *Bikram Nayak
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private Long customerId;
